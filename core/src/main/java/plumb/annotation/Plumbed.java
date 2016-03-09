7eb90150f36b01cc6aa5b409ceb3943a79751028
Kevin Carpenter <kevinjcarpenter@gmail.com>
2016-03-09 10:31:18 -0500

    Update core annotations

--- core/src/main/java/plumb/annotation/Plumbed.java
+++ core/src/main/java/plumb/annotation/Plumbed.java
@@ -1,6 +1,12 @@
 package plumb.annotation;
 
-public @interface Plumbed {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface Plumbed {
     Class value();
 }
