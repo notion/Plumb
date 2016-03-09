67e2b5c9fe2b122003f77cb9d978672297714ead
Kevin Carpenter <kevinjcarpenter@gmail.com>
2016-03-09 10:31:18 -0500

    Update core annotations

--- core/src/main/java/plumb/annotation/In.java
+++ core/src/main/java/plumb/annotation/In.java
@@ -1,6 +1,12 @@
 package plumb.annotation;
 
-public @interface In {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.FIELD)
+public @interface In {
     String value();
 }
