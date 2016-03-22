af66ed52cbcf8af9e19f20898e8cda1167066091
Kevin Carpenter <kevinjcarpenter@gmail.com>
2016-03-22 16:24:31 -0400

    Make ColorPickerViewModel.color a method
    This change is solely to prove that @Out-annotated
    elements can be methods.

--- rxjoin-example/src/main/java/rxjoin/example/viewmodel/ColorPickerViewModel.java
+++ rxjoin-example/src/main/java/rxjoin/example/viewmodel/ColorPickerViewModel.java
@@ -12,10 +12,12 @@ public class ColorPickerViewModel {
     @In("green") public BehaviorSubject<Integer> green = BehaviorSubject.create();
     @In("blue") public BehaviorSubject<Integer> blue = BehaviorSubject.create();
 
-    @Out("color") public Observable<Integer> color = Observable.combineLatest(red, green, blue,
-            new Func3<Integer, Integer, Integer, Integer>() {
-                @Override public Integer call(Integer r, Integer g, Integer b) {
-                    return (0xFF << 24) | (r << 16) | (g << 8) | b;
-                }
-            });
+    @Out("color") public Observable<Integer> colorMethod() {
+        return Observable.combineLatest(red, green, blue,
+                new Func3<Integer, Integer, Integer, Integer>() {
+                    @Override public Integer call(Integer r, Integer g, Integer b) {
+                        return (0xFF << 24) | (r << 16) | (g << 8) | b;
+                    }
+                });
+    }
 }
